6d36de95e31b42d56ceb007a175d81348dcb5bac
alfhaos <alfhaos@naver.com>
2022-12-27 21:04:53 +0900

    카프카 관리 프로그램

---
+++ src/main/java/com/kafka/administrate/KafkaAdApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.administrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/administrate/KafkaAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.administrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
